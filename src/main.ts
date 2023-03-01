6d3d6bc332acca147d0ae73a9c2b8e710d37c513
timli1020 <zhaotian.li@uconn.edu>
2023-03-01 00:08:42 -0500

    adds app json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { FruitsModule } from './fruits/fruits.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(FruitsModule);
   await app.listen(3000);
